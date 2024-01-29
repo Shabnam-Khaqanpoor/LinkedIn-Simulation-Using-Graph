fa43a41d54c36223ddca36fefeecca29e6f1dfe8
Shabnam2003 <khaghanpourshabnam@gmail.com>
2024-01-29 14:39:58 +0330

    add setter and getter to User class.

--- src/main/java/com/example/socialnetwork/connections/User.java
+++ src/main/java/com/example/socialnetwork/connections/User.java
@@ -19,4 +19,44 @@ class User {
         this.connections = connections;
         this.priority=priority;
     }
+
+    public void setGeneralInfo(Map<String, String> generalInfo) {
+        this.generalInfo = generalInfo;
+    }
+
+    public void setSpecializedInfo(Map<String, String> specializedInfo) {
+        this.specializedInfo = specializedInfo;
+    }
+
+    public void setSkills(LinkedList<String> skills) {
+        this.skills = skills;
+    }
+
+    public void setConnections(LinkedList<Integer> connections) {
+        this.connections = connections;
+    }
+
+    public void setPriority(LinkedList<Integer> priority) {
+        this.priority = priority;
+    }
+
+    public Map<String, String> getGeneralInfo() {
+        return generalInfo;
+    }
+
+    public Map<String, String> getSpecializedInfo() {
+        return specializedInfo;
+    }
+
+    public LinkedList<String> getSkills() {
+        return skills;
+    }
+
+    public LinkedList<Integer> getConnections() {
+        return connections;
+    }
+
+    public LinkedList<Integer> getPriority() {
+        return priority;
+    }
 }
